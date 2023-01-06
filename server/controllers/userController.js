ecae13dc35a3b388f0d3e388d5491bb1c17f1adf
Chihiro Shibayama <chihiro.shibayama@gmail.com>
2023-01-05 22:00:55 -0800

    started adding login & signup functionality

---
+++ server/controllers/userController.js
@@ -0,0 +1,3 @@
+// login user
+
+// signup user
---
+++ server/routes/user.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+// Log in route
+router.post('/login', () => {});
+
+// Sign up route
+router.post('/signup', () => {});
+
+module.exports = router;
